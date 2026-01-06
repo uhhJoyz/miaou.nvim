d676633ce5a8c04e22594cdcb0f04e090d32df43
uhhJoyz <wcb8ze@virginia.edu>
2026-01-06 00:16:30 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    miaou.url = "github:uhhJoyz/miaou.nvim";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    miaou,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem = let
+        meows = import ./meows.nix;
+      in
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }: {
+          packages = {
+            default = miaou.lib.miaouCustom {
+              inherit meows;
+              inherit system;
+            };
+          };
+        };
+    };
+}
---
+++ meows.nix
@@ -0,0 +1,61 @@
+[
+  # -------- user experience --------
+  /nvim-help # get help with keybinds (it's dangerous to go alone)
+  /training-wheels # get (mildly annoying) training wheels to help you learn keybinds
+  /qol # general changes you'd miss if they weren't there
+  /nice-ui # a better UI for a better editor
+  /dashboard # stylish dashboard for UX
+  /easy-parenthesis # never press the other parenthesis
+  /autocomplete # you'll never have the last word
+  /snippets # way way way way way less boilerplate
+  /file-browsing # the power of the system at your fingertips
+  /autocenter # it's like the buffer revolves around you
+  /spellcheck # me monkey, machine do grammar
+  /glass-ui # 🫥
+
+  # -------- navigation --------
+  /better-find # upgrades the f/F and t/T keys (and <c-o> / <c-i>)
+  /hop # jump to anywhere you like!
+  /search # search with the power of a billion horses
+
+  # -------- misc. utilities --------
+  /git # all your code in one place (until they ruin their image more)
+  /pickers # a color-picker and an icon picker 󰈸 / #04c804 / hsl(120 96% 40%)
+  /window-management # manage windows within myow
+  /notes # keep a second mind in your myow installation! (called mind)
+  /zen-mode # quiet your mind
+  /screenshot # but first, let me take a selfie FIXME: not working!
+
+  # -------- for fun! --------
+  /minimap # a for-fun minimap of your code
+  /leetcode # leetcode *inside* of nvim!
+  /multicursor # you seem responsible enough to clone yourself... right?
+  /discord # flex on your friends in discord e-gangster fashion
+  /ultimeow # your very own supermeowers
+
+  # -------- themes --------
+  /theme/catppuccin/mocha
+
+  # -------- language support --------
+  # nix is enabled by default to support this configuration
+  /debugging # little elves help guide your coding
+  # /lang/english # harness the power of the machine lord to write... english?
+  /lang/typst # the superior markup language
+  /lang/ocaml # best language ever
+  /lang/markdown # take notes or whatever the kids do these days
+  /lang/bash # it's scriptin' time
+  /lang/clang # C and C++, the classics
+  /lang/webdev # html, css, json, sql, ts (no js yet)
+  /lang/php # the facebook language or something
+  /lang/java # a nice hot cup of sadness
+  /lang/kotlin # java but better
+  /lang/go # simple but effective (and garbage collected)
+  /lang/lua # for roblox or vim but definitely not both
+  /lang/zig # C+++++
+  /lang/python # big tech's favorite treat
+  /lang/rust # fast AND memory safe?
+  /lang/asm # back to the basics
+  /lang/haskell # true classic functional programming
+
+  # /meow-meow-mode # not for the faint of heart - beware!
+]
